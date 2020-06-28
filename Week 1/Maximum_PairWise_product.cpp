a1c81fcd3471eb782a24c142385ed0668293b753
Muhammad Al-Barham <mohammedbrham98@gmail.com>
2020-06-28 21:04:45 +0300

    Add

---
+++ Week 1/Maximum_PairWise_product.cpp
@@ -0,0 +1,73 @@
+
+#include <iostream>
+#include <vector>
+
+/* 
+long long Maixmum_Pairwise_Product(const std::vector<int>& numbers){
+
+
+    long long result = 0 ; 
+    int n = numbers.size() ; 
+    
+    for(int i=0;i<n;++i){
+
+        for(int j=i+1; j<n ; ++j){
+
+            if( ( (long long )numbers[i] ) * numbers[j] > result) 
+                result = ( ( long long )numbers[i] ) * numbers[j] ; 
+        }
+
+    }
+
+    return result ; 
+}
+
+*/ 
+
+
+long long Maixmum_Pairwise_Product(const std::vector<int>& numbers){
+
+int n = numbers.size() ; 
+
+int max_index_1 = 0 ; 
+
+for(int i=0 ; i<n ; ++i){
+
+    if( ( max_index_1 == -1 ) || ( numbers[i] > numbers[max_index_1])  )
+        max_index_1 = i ; 
+}
+
+int max_index_2 = 0 ; 
+
+
+for(int i = 0 ; i<n ; ++i ){
+
+if( ( ( max_index_2 == -1)  || numbers[i] > numbers[max_index_2] ) && (numbers[i] != numbers[max_index_1] ) ) 
+
+    max_index_2 = i ; 
+
+}
+
+
+return  (  ( (long long )numbers[max_index_2] )  * numbers[max_index_1] ) ; 
+
+}
+
+int main() {
+
+    int n ; 
+    std::cin >> n ;
+ 
+    std::vector<int> numbers(n) ;
+    
+    for(int j = 0 ; j<n ; ++j)
+        std::cin >> numbers[j] ; 
+
+
+    long long result = Maixmum_Pairwise_Product(numbers) ; 
+
+    std::cout  << result << "\n" ; 
+
+    return 0 ; 
+}
+
---
+++ Week 1/Sum_Of_Two_Digits.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+
+
+int main(){
+
+    int a,b, sum = 0 ; 
+
+    std::cin >> a >> b ; 
+
+    std::cout << a + b << std::endl ; 
+
+    return 0 ;
+}
